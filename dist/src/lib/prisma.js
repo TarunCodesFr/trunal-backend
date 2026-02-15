f03b5ae5dcd9f8969c8e8f4d62ad227733458a41
TarunCodes <akurax4@gmail.com>
2026-02-15 16:25:01 +0530

    prisma issue

--- dist/src/lib/prisma.js
+++ dist/src/lib/prisma.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
+const client_1 = require("../generated/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 const adapter = new adapter_pg_1.PrismaPg({
     connectionString: process.env.DATABASE_URL
